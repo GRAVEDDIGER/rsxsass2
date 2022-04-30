ce32209bd8d6ccc4af8e972155ad0da3f7a1d316
Adrian Daniel Abadin <aabadin@gmail.com>
2022-04-30 06:36:46 -0300

    Trabajando en dbt.html en zona de descargas

--- dist/js/estilo.d8b4bf80.js
+++ dist/js/estilo.d8b4bf80.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64482" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65424" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/maps/estilo.d8b4bf80.js.map
+//# sourceMappingURL=/estilo.d8b4bf80.js.map
